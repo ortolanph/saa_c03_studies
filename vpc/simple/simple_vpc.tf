f8db74001652ff4af31ec39013d72fae05a4e7a5
Paulo Ortolan <paulo.ortolan@gmail.com>
2023-07-19 22:36:10 +0100

    Modifying some resources

--- vpc/simple/simple_vpc.tf
+++ vpc/simple/simple_vpc.tf
@@ -1,6 +1,12 @@
 resource "aws_vpc" "simple_vpc" {
   cidr_block = "172.16.0.0/16"
 
+  tags = {
+    Name      = "${var.workspace}-simple-vpc"
+    workspace = var.workspace
+    exam      = "saa_c03"
+  }
+
   tags_all = {
     Name      = "${var.workspace}-simple-vpc"
     workspace = var.workspace
@@ -12,7 +18,14 @@ resource "aws_subnet" "simple_subnet" {
   vpc_id = aws_vpc.simple_vpc.id
 
   cidr_block = "172.16.100.0/24"
-  availability_zone = "eu-west-1"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "${var.workspace}-simple-subnet"
+    workspace = var.workspace
+    exam      = "saa_c03"
+  }
 
   tags_all = {
     Name      = "${var.workspace}-simple-subnet"
@@ -26,6 +39,12 @@ resource "aws_network_interface" "simple_network_interface" {
 
   private_ip = "172.16.100.99"
 
+  tags = {
+    Name      = "${var.workspace}-simple-network-interface"
+    workspace = var.workspace
+    exam      = "saa_c03"
+  }
+
   tags_all = {
     Name      = "${var.workspace}-simple-network-interface"
     workspace = var.workspace
@@ -36,6 +55,12 @@ resource "aws_network_interface" "simple_network_interface" {
 resource "aws_internet_gateway" "simple_internet_gateway" {
   vpc_id = aws_vpc.simple_vpc.id
 
+  tags = {
+    Name      = "${var.workspace}-simple-internet-gateway"
+    workspace = var.workspace
+    exam      = "saa_c03"
+  }
+
   tags_all = {
     Name      = "${var.workspace}-simple-internet-gateway"
     workspace = var.workspace
@@ -44,7 +69,7 @@ resource "aws_internet_gateway" "simple_internet_gateway" {
 }
 
 resource "aws_route" "simple_route" {
-  route_table_id = var.route_table_id
+  route_table_id = aws_vpc.simple_vpc.main_route_table_id
 
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.simple_internet_gateway.id
